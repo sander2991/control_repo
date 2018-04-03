9dd8db48bbda7f5008cabf74260d9f605ad60e95
sander2991 <37831672+sander2991@users.noreply.github.com>
2018-04-03 16:15:00 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,7 @@ class minecraft (
   file {$install_dir:
     ensure => directory,
   }
-  file {"{$install_dir}/minecraft_server.jar":
+  file {"${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
     before => Service['minecraft'],
@@ -17,7 +17,7 @@ class minecraft (
   package {'java':
     ensure => present,
   }
-  file {"{$install_dir}/eula.txt":
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -30,6 +30,6 @@ class minecraft (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
   }
 }
