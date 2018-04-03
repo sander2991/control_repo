b634bd576cf93b61509580cf8904ec94494222e9
sander2991 <37831672+sander2991@users.noreply.github.com>
2018-04-03 16:03:37 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,12 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+  $install_dir = '/opt/minecraft'
+  file {$install_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {"{$install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    source => $url
     before => Service['minecraft'],
   }
   file {'/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7':
@@ -14,7 +16,7 @@ class minecraft {
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"{$install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -25,6 +27,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']]
+    require => [Package['java'],File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
   }
 }
